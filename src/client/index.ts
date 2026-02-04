509bbb03a454d6eb1ca93e7ac4f90617d6d009fe
ArturT995 <arturtre95@gmail.com>
2026-02-04 07:33:39 +0000

    added more clientside commands and a durable queue to peril_topic exchange

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,20 +1,29 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js"
+import { clientWelcome, 
+  getInput, 
+  printClientHelp,
+  getMaliciousLog,
+  printQuit,
+  commandStatus,
+} from "../internal/gamelogic/gamelogic.js"
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/publish.js"
-
-
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js"
-import type { PlayingState } from "../internal/gamelogic/gamestate.js"
+import { GameState } from "../internal/gamelogic/gamestate.js"
+import { commandSpawn } from "../internal/gamelogic/spawn.js"
+import { commandMove } from "../internal/gamelogic/move.js"
+
 
 
 async function main() {
   console.log("Starting Peril client...");
   let cString = "amqp://guest:guest@localhost:5672/";
 
-
+  
   const conn = await amqp.connect(cString);
   const username = await clientWelcome();
   const queueName = `${PauseKey}.${username}`;
+  
+  
 
   await declareAndBind(
     conn,
@@ -22,8 +31,44 @@ async function main() {
     queueName,
     PauseKey,
     SimpleQueueType.Transient,
-  )
+  );
 
+
+  const gs = new GameState(username)
+  
+  while (true) {
+    const words = await getInput();
+    if (words.length === 0) continue;
+    const command = words[0];
+    
+    if (command === "spawn") {
+      try {
+        commandSpawn(gs, words);
+      } catch (err) {
+        console.log((err as Error).message);
+      }
+    } else if (command === "move") {
+      try {
+        commandMove(gs, words);
+      } catch (err) {
+        console.log((err as Error).message);
+      }
+    } else if (command === "status") {
+      commandStatus(gs)
+    } else if (command === "help") {
+      printClientHelp()
+
+    } else if (command === "spam") {
+      console.log("Spamming not allowed yet!")
+      //getMaliciousLog()
+    } else if (command === "quit") {
+      printQuit()
+      process.exit(0);
+    } else {
+      console.log("Invalid command")
+      continue;
+    }
+  }
 
 };
 
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,64 +1,68 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js"
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js"
+import { ExchangePerilDirect, PauseKey, RoutingKey, ExchangePerilTopic, GameLogSlug } from "../internal/routing/routing.js"
 import type { PlayingState } from "../internal/gamelogic/gamestate.js"
 import { printServerHelp, getInput } from "../internal/gamelogic/gamelogic.js"
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/publish.js"
+
 
 async function main() {
   
   console.log("Starting Peril server...");
+  
   let cString = "amqp://guest:guest@localhost:5672/";
   
   const conn = await amqp.connect(cString);
   console.log("connection was succesful")
   
   const confirm = await conn.createConfirmChannel();
-
-  const state: PlayingState = {
-  isPaused: true,
-  };
-
-  const gamestate = await publishJSON(
-    confirm,
-    ExchangePerilDirect,
-    PauseKey,
-    state,
-  )
-
-  printServerHelp();
-
-  let shouldRun = 0;
-
-  while (shouldRun < 1) {
-    const input = await getInput();
-    if (input.length === 0){
-      continue
-    }
-    else if (input[0] === "pause") {
-      console.log("Sending pause message.")
-      gamestate
-    }
-    else if (input[0] === "resume") {
-      console.log("Sending resume message.")
-      state.isPaused = false
-      gamestate
-    }
-    else if (input[0] === "quit") {
-      console.log("Exiting.")
-      break
-    }
-    else {
-      console.log("Invalid command")
-      continue
-    }
-
-  };
-
+  
   process.on('SIGINT', () => {
     console.log(`Shutting down...`);
     conn.close();
     process.exit(0);
   });
+
+  const state: PlayingState = {
+  isPaused: true,
+  };
+  
+
+  const RouteKey = `${GameLogSlug}.${RoutingKey}`;
+  await declareAndBind(
+      conn,
+      ExchangePerilTopic,
+      GameLogSlug,
+      RouteKey,
+      SimpleQueueType.Durable,
+    );
+  
+
+  printServerHelp();
+
+  while (true) {
+    const input = await getInput();
+    if (input.length === 0) continue;
+      
+    else if (input[0] === "pause") {
+      console.log("Sending pause message.")
+      state.isPaused = true;
+      await publishJSON(confirm, ExchangePerilDirect, PauseKey, state);
+    }
+    else if (input[0] === "resume") {
+      console.log("Sending resume message.")
+      state.isPaused = false
+      await publishJSON(confirm, ExchangePerilDirect, PauseKey, state);
+    }
+    else if (input[0] === "quit") {
+      console.log("Exiting.")
+      process.exit(0);
+    }
+    else {
+      console.log("Invalid command")
+      continue
+    }
+  };
 }
 
 main().catch((err) => {
